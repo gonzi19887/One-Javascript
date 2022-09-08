9f9e0b903fb086e6f27e9c3d9c2a5fdcb6c545bc
Programación One <oscarmarley1988@gmail.com>
2022-09-08 10:29:02 +0000

    Eliminar Pacientes y su animacion

---
+++ js/eliminarPaciente.js
@@ -0,0 +1,10 @@
+var pacientes = document.querySelectorAll(".paciente");
+
+var tabla = document.querySelector("#tabla-pacientes");
+
+tabla.addEventListener("dblclick", function(event) {
+	event.target.parentNode.classList.add("fadeOut");
+	setTimeout(function() {
+		event.target.parentNode.remove();
+	}, 300);
+});
